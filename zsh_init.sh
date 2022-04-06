dccce28f42fa121c9b2ec4406b9c9879f48484e0
imlcs <35558923+imlcs@users.noreply.github.com>
2022-04-06 15:38:43 +0800

    Update and rename function.sh to zsh_init.sh

---
+++ zsh_init.sh
@@ -0,0 +1,12 @@
+apt install -y zsh unzip zip
+
+mv $HOME/.vim $HOME/.vimrc /tmp
+
+wget https://github.com/imlcs/vim/archive/refs/heads/master.zip -O $HOME/vim.zip  && unzip $HOME/vim.zip && rm -f $HOME/vim.zip
+
+
+mv $HOME/vim-master/.vim $HOME/vim-master/.vimrc $HOME && rm -fr $HOME/vim-master
+
+wget https://github.com/imlcs/zsh/archive/refs/heads/master.zip -O $HOME/zsh.zip  && unzip $HOME/zsh.zip && rm -f $HOME/zsh.zip
+
+mv $HOME/zsh-master/.zshrc $HOME/zsh-master/.oh-my-zsh $HOME && /bin/rm -fr $HOME/zsh-master
